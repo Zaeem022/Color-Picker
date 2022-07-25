7cb3b3e0ea3d040ef15d01e087868961e32c1eb2
Zaeem Raza <zaeemraza1063@gmail.com>
2022-07-25 14:40:59 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function func1(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Red';
+    document.getElementById('main').style.background='red';
+    document.getElementById('main').style.color='WHITE';
+}
+function func2(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Blue';
+    document.getElementById('main').style.background='Blue';
+    document.getElementById('main').style.color='WHITE';
+}
+function func3(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Green';
+    document.getElementById('main').style.background='Green';
+    document.getElementById('main').style.color='WHITE';
+}
+function func4(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Yellow';
+    document.getElementById('main').style.background='yellow';
+    document.getElementById('main').style.color='blue';
+}
+function func5(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Black';
+    document.getElementById('main').style.background='black';
+    document.getElementById('main').style.color='white';
+}
+function func6(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Grey';
+    document.getElementById('main').style.background='grey';
+    document.getElementById('main').style.color='white';
+}
+function func7(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='White';
+    document.getElementById('main').style.background='white';
+    document.getElementById('main').style.color='black';
+}
+function func8(){
+    document.getElementById('main').style.visibility='visible';
+    document.getElementById('main').innerHTML='Gold';
+    document.getElementById('main').style.background='gold';
+    document.getElementById('main').style.color='red';
+}
